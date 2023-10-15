a9948f5e1fe35715fb90279bda314a9b719c51b3
GeetaPoladas <112972730+GeetaPoladas@users.noreply.github.com>
2023-10-15 12:50:02 +0530

    Add files via upload

---
+++ questionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.quizapp;
+
+public class questionAnswer {
+
+    public static String questionss[]= {
+            "What is the capital of india?",
+            "What planet is closest to the sun?",
+            "What is the only continent with land in all four hemispheres?",
+            "How many hearts does an octopus have?",
+            "What is the richest temple in India?",
+            "Who was the first Disney princess?",
+            "Who is the first Indian to win a Nobel Prize?",
+            "What company was originally called 'Cadabra'?",
+            "How many bones do we have in an ear?",
+            "What is a group of crows called?"
+    };
+
+    public static  String choices[][] = {
+            { "Chennai" , "Delhi" , "Hyderabad" , "Mumbai"},
+            {"Mars" , "Saturn" , "Earth"  , "Mercury"} ,
+            {"Asia","Africa","Australia","Europe"},
+            {"one" , "two" , "three" , "four" },
+            {"Tirupati" , "Golden Temple" , "Padmanabhaswamy" , "Shirdi Sai Baba"},
+            {"Snow White","Jasmine","Cindrella","Moana"},
+            {"Mother Teresa" , "Sir C V Raman" , "Rabindranath Tagore" , "Har Gobind Khorana"},
+            {"Google","Facebook","Amazon","Twitter"},
+            {"4","3","2","1"},
+            {"Kettle","Parliament","Streak","Murder"}
+    };
+
+    public static String CorrectAnswer[] = {
+            "Delhi",
+            "Mercury",
+            "Africa",
+            "three",
+            "Padmanabhaswamy",
+            "Snow White",
+            "Rabindranath Tagore",
+            "Amazon",
+            "3",
+            "Murder"
+    };
+}
